b1e6917cf6552357284555669b82a180dff85bec
joseguilhrm <guilhrmjose@gmail.com>
2025-05-02 16:28:16 -0300

    Implementando lista, atualizar e excluir registro incidentes

--- app/src/main/java/com/example/projetointegrador/Event.kt
+++ app/src/main/java/com/example/projetointegrador/Event.kt
@@ -1,6 +1,7 @@
 package com.example.projetointegrador
 
 data class Event(
+    val docId : String,
     val title: String,
     val description: String,
     val imageResId: Int
--- app/src/main/java/com/example/projetointegrador/ViewEventsActivity.kt
+++ app/src/main/java/com/example/projetointegrador/ViewEventsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.projetointegrador
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import android.widget.Button
@@ -11,8 +12,17 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ViewEventsActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+    private val incidentes = mutableListOf<Event>()
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var tvTotal: TextView
+    private lateinit var adapter: EventAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +31,8 @@ class ViewEventsActivity : AppCompatActivity() {
 
         window.statusBarColor = ContextCompat.getColor(this, R.color.button_enabled)
 
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.register_incident)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -32,30 +44,130 @@ class ViewEventsActivity : AppCompatActivity() {
             finish()
         }
 
-        // Dados mockados
-        val mockEvents = listOf(
-            Event("Broken Projector", "Projector in room 103 not working", R.drawable.projetor),
-            Event("Loose Cable", "Cable near entrance is loose", R.drawable.projetor),
-            Event("No Board Marker", "No marker available in lab 204", R.drawable.projetor)
-        )
-
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerEvents)
+        tvTotal = findViewById<TextView>(R.id.tvTotal)
+        recyclerView = findViewById(R.id.recyclerEvents)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = EventAdapter(
-            mockEvents,
+
+        adapter = EventAdapter(
+            incidentes,
             onEditClick = { event ->
                 val dialog = EditEventDialogFragment(event) { updatedEvent ->
-                    Toast.makeText(this, "Updated: ${updatedEvent.title}", Toast.LENGTH_SHORT).show()
-                    // Aqui você atualizaria a lista e notificaria o adapter se necessário
+                    atualizarDocIncidente(updatedEvent)
                 }
                 dialog.show(supportFragmentManager, "editEvent")
             },
             onDeleteClick = { event ->
-                Toast.makeText(this, "Delete: ${event.title}", Toast.LENGTH_SHORT).show()
+                deletarDocIncidente(event)
             }
         )
 
-        val tvTotal = findViewById<TextView>(R.id.tvTotal)
-        tvTotal.text = "Total: ${mockEvents.size} Events"
+        recyclerView.adapter = adapter
+
+        preencherListaIncidentes()
+    }
+
+    private fun deletarDocIncidente(incidente: Event) {
+        val idUsuarioLogado = auth.currentUser?.uid
+
+        if (idUsuarioLogado != null) {
+            val referenciaIncidente = db
+                .collection("Usuarios")
+                .document(idUsuarioLogado)
+                .collection("Incidentes")
+                .document(incidente.docId)
+
+            referenciaIncidente.delete()
+                .addOnSuccessListener {
+                    Log.d("Firestore", "Documento excluído com sucesso!")
+                    Toast.makeText(this, "Documento excluído com sucesso!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                .addOnFailureListener { e ->
+                    Log.e("Firestore", "Erro ao excluir documento: ${e.message}", e)
+                    Toast.makeText(this, "Erro ao excluir documento", Toast.LENGTH_SHORT)
+                        .show()
+                }
+        }
+    }
+
+    private fun atualizarDocIncidente(incidente: Event) {
+        val idUsuarioLogado = auth.currentUser?.uid
+
+        if (idUsuarioLogado != null) {
+            val referenciaIncidente = db
+                .collection("Usuarios")
+                .document(idUsuarioLogado)
+                .collection("Incidentes")
+                .document(incidente.docId)
+
+            if (incidente.title.isEmpty() && incidente.description.isEmpty()) {
+                return
+            }
+
+            val updates = mutableMapOf<String, Any>()
+
+            if (incidente.title.isNotEmpty()) {
+                updates["titulo"] = incidente.title
+            }
+
+            if (incidente.description.isNotEmpty()) {
+                updates["descricao"] = incidente.description
+            }
+
+            referenciaIncidente.update(updates)
+                .addOnSuccessListener {
+                    Log.d("Firestore", "Documento atualizado com sucesso!")
+                    Toast.makeText(
+                        this,
+                        "Documento atualizado com sucesso!",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+                .addOnFailureListener { e ->
+                    Log.e("Firestore", "Erro ao atualizar documento: ${e.message}", e)
+                    Toast.makeText(this, "Erro ao atualizar documento", Toast.LENGTH_SHORT)
+                        .show()
+                }
+        }
+    }
+
+    private fun preencherListaIncidentes() {
+        val idUsuarioLogado = auth.currentUser?.uid
+
+        if (idUsuarioLogado != null) {
+
+            val referenciaIncidentes = db
+                .collection("Usuarios")
+                .document(idUsuarioLogado)
+                .collection("Incidentes")
+
+            referenciaIncidentes.addSnapshotListener { querySnapshot, erro ->
+                val docsIncidentes = querySnapshot?.documents
+
+                incidentes.clear()
+
+                docsIncidentes?.forEach { documentSnapshot ->
+                    val docIncidente = documentSnapshot?.data
+                    val idDocIncidente = documentSnapshot?.id
+
+                    if (docIncidente != null && idDocIncidente != null) {
+                        val titulo = docIncidente["titulo"] as? String ?: ""
+                        val descricao = docIncidente["descricao"] as? String ?: ""
+                        val imagePath = R.drawable.projetor
+
+                        incidentes.add(
+                            Event(
+                                idDocIncidente, titulo, descricao, imagePath
+                            )
+                        )
+                    }
+                }
+
+                adapter.notifyDataSetChanged()
+
+                tvTotal.text = "Total: ${incidentes.size} Events"
+            }
+        }
     }
 }
